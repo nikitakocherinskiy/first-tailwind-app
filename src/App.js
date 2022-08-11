6ed8e44e62f6a153e9e799bac41458caf9f28628
nikitakocherinskiy <nik.koch.67@gmail.com>
2022-08-11 15:41:42 +0300

    init: load tailwind project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import styles from './App.module.scss'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
+	const [isOpen, setIsOpen] = useState(false)
+
+	return (
+		<div className={styles.parent}>
+			<h1 className={styles.heading}>Hello World!</h1>
+			<div>
+				<input
+					type='text'
+					placeholder='Email'
+					value={email}
+					onChange={e => setEmail(e.target.value)}
+				/>
+				<input
+					type='text'
+					placeholder='Password'
+					value={password}
+					onChange={e => setPassword(e.target.value)}
+				/>
+				<button className={styles.button} onClick={() => setIsOpen(!isOpen)}>
+					Login
+				</button>
+				{isOpen && <button className='animate-fade'>TEST</button>}
+			</div>
+		</div>
+	)
 }
 
-export default App;
+export default App
